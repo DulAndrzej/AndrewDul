b05e8e7ab808840fa3261e64e965e7c9b00f5cf9
DulAndrzej <dulandrzej.js@gmail.com>
2023-05-07 21:53:53 +0100

    I removed unnecessary files and provided three js to the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ThreeExample from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThreeExample />
+    <h1>Please wait a little longer for this masterpiece</h1>
   </React.StrictMode>
 );
 
